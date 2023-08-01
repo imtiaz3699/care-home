f769a907bf345c32750e64a1abcd6480851691a5
imtiaz3699 <imtiazali78652@gmail.com>
2023-07-31 22:47:47 -0700

    first commit

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navbar () {
+    <>
+    <div className='flex flex-row items-center justify-between bg-green-500 h-[55px] w-full'>
+        
+    </div>
+    
+    </>
+}
