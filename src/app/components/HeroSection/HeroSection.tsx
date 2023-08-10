e8dc4ee6bd42a1dc908501bcd232b1a1db373306
imtiaz3699 <imtiazali78652@gmail.com>
2023-08-10 05:57:37 -0700

    last commit

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -210,17 +210,11 @@ const testimonials = [
 
 
     </div>
-    <div className="bg-green-800 text-white px-5 md:px-20 flex justify-center py-20  flex flex-col items-center text-center ">
-<div className="text-[16px] lg:text-[25px] max-w-[1000px] flex flex-col items-center justify-center"><span className="text-[25px] md:text-[35px] lg:text-[50px] font-bold">Who we are</span> 
-   </div>
-   <div className="max-w-[1000px] text-[30px]">
-   Founded with a vision to redefine aging, SBH Health Care has been a trusted refuge for seniors seeking a safe and welcoming environment. Our legacy of care spans years, during which we have nurtured countless lives, forming lasting bonds and enriching the lives of each resident who becomes a part of our extended family.
-   </div>
-</div>
-<div className="flex flex-col items-center justify-center bg-yellow-100 gap-4 sm:gap-10 py-5 md:py-20 md:px-10 md:px-0">
+    
+<div className="flex flex-col items-center justify-center bg-green-800 gap-4 sm:gap-10 py-5 md:py-20 md:px-10 md:px-0">
         <div className="flex flex-col items-center gap-3 ">
-        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="41.9" viewBox="0 0 42 41.9">
-  <path fill="green" d="M38.5,30.25V14.15L28,6.8,17.5,14.15V20.6h-3V12.65L28,3l13.5,9.65v17.6ZM28,6.8ZM29,17h2V15H29Zm-4,0h2V15H25Zm4,4h2V19H29Zm-4,0h2V19H25Zm3.3,23.9L12.75,40.45V43.3H2V23.6H17.45L30.2,28.4a5.267,5.267,0,0,1,2.275,1.625A4.958,4.958,0,0,1,33.4,33.25h5.7a4.589,4.589,0,0,1,3.5,1.5A5.7,5.7,0,0,1,44,38.8v1.3ZM5,40.3H9.7V26.6H5Zm23.1,1.5,12.8-3.9a2.619,2.619,0,0,0-.75-1.3,1.661,1.661,0,0,0-1.05-.35H28.75a17.454,17.454,0,0,1-5.2-.75L19.5,34.25l1.1-2.9,3.65,1.2a13.807,13.807,0,0,0,2.375.55,30.135,30.135,0,0,0,3.575.15,3.2,3.2,0,0,0-.225-1.175,1.452,1.452,0,0,0-.775-.825L16.95,26.6h-4.2V37.3ZM9.7,33.45ZM30.2,33.25ZM9.7,33.45ZM12.75,33.45Z" transform="translate(-2 -3)"></path>
+        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="50" viewBox="0 0 42 41.9">
+  <path fill="black" d="M38.5,30.25V14.15L28,6.8,17.5,14.15V20.6h-3V12.65L28,3l13.5,9.65v17.6ZM28,6.8ZM29,17h2V15H29Zm-4,0h2V15H25Zm4,4h2V19H29Zm-4,0h2V19H25Zm3.3,23.9L12.75,40.45V43.3H2V23.6H17.45L30.2,28.4a5.267,5.267,0,0,1,2.275,1.625A4.958,4.958,0,0,1,33.4,33.25h5.7a4.589,4.589,0,0,1,3.5,1.5A5.7,5.7,0,0,1,44,38.8v1.3ZM5,40.3H9.7V26.6H5Zm23.1,1.5,12.8-3.9a2.619,2.619,0,0,0-.75-1.3,1.661,1.661,0,0,0-1.05-.35H28.75a17.454,17.454,0,0,1-5.2-.75L19.5,34.25l1.1-2.9,3.65,1.2a13.807,13.807,0,0,0,2.375.55,30.135,30.135,0,0,0,3.575.15,3.2,3.2,0,0,0-.225-1.175,1.452,1.452,0,0,0-.775-.825L16.95,26.6h-4.2V37.3ZM9.7,33.45ZM30.2,33.25ZM9.7,33.45ZM12.75,33.45Z" transform="translate(-2 -3)"></path>
 </svg>
           <p className="text-[36px] font-bold">Welcome to SBH Healthcare</p>
         </div>
@@ -230,10 +224,16 @@ const testimonials = [
         whatever we can to assist you and your loved ones with settling into your new home. We warmly welcome prospective 
         residents and their loved ones to come and take a tour of our care.</div>
     </div>
-
-        <div className="flex flex-col items-center justify-center py-5 bg-green-800">
-        <div className="text-[20px] md:text-[30px] font-bold text-green-500 text-center">Our Care Services</div>
-        <div className="text-[16px] md:text-[20px] px-3 md:px-0 md:w-[50%] text-center ">"We collaborate intimately with our residents, their cherished ones, and every invested party to ensure that we provide unwavering assistance for individuals to reside with us as integral members of our extended kinship."</div>
+    <div className="bg-yellow-100 text-black px-5 md:px-20 flex justify-center py-20 flex flex-col items-center text-center ">
+<div className="text-[16px] lg:text-[25px] max-w-[1000px] flex flex-col items-center justify-center"><span className="text-[25px] md:text-[35px] lg:text-[50px] font-bold">Who we are</span> 
+   </div>
+   <div className="max-w-[1000px] text-[30px]">
+   Founded with a vision to redefine aging, SBH Health Care has been a trusted refuge for seniors seeking a safe and welcoming environment. Our legacy of care spans years, during which we have nurtured countless lives, forming lasting bonds and enriching the lives of each resident who becomes a part of our extended family.
+   </div>
+</div>
+<div className="flex flex-col items-center justify-center py-20 bg-green-800">
+        <div className="text-[20px] md:text-[36px] font-bold  text-center text-black">Our Care Services</div>
+        <div className="text-[16px] md:text-[30px] px-3 md:px-0 md:w-[50%] text-center ">"We collaborate intimately with our residents, their cherished ones, and every invested party to ensure that we provide unwavering assistance for individuals to reside with us as integral members of our extended kinship."</div>
         <div className="px-3 grid grid-cols-2 md:flex md:flex-row items-center justify-between gap-3 md:gap-20 py-10">
            {
             ourServices.map((element,idx)=> {
@@ -247,10 +247,7 @@ const testimonials = [
         </div>
        </div>
 
-    {/* <div className=" flex flex-col items-center text-center gap-5 md:flex-row md:justify-between w-full px-4 md:px-12 bg-white py-8  ">
-    <div className="font-bold text-[25px] md:text-[35px]">Quality Care Your Loved Ones Deserve</div>
-    <div className="bg-green-800 p-2 md:p-5 rounded-xl font-bold text-[15px]">Call Us Today: <span>07916789486</span></div>
-    </div> */}
+    
     <div className="flex flex-col items-center pb-10 gap-5 lg:gap-10">
       <h1 className="text-[25px] sm:text-[35px] text-center font-bold text-blue-800 mt-10">Why Choose us</h1>
       <div className="flex flex-col-reverse lg:flex-row px-3 sm:px-0 items-center gap-2 lg:gap-10">
@@ -338,49 +335,6 @@ Life at SBH is a tapestry of vitality and connection. Our diverse activities, fr
    Our care home is a place of comfort, love, and joy, where residents thrive and families are always welcomed.</div>
 </div>
 </div>
-
-
-
-{/* <div className="flex flex-row items-center justify-center px-3 md:px-10 py-10 md:gap-20"> 
-<div className="flex flex-col items-center justify-center w-full  md:w-[500px] border border-green-500 rounded-xl gap-5 px-5 md:px-10 py-10">
-<div className="flex flex-col gap-1 md:gap-3 w-full"> 
-  <label htmlFor="name" className="text-[16px] md:text-[20px]">Your Name</label>
-  <input type="text" className="w-full h-[30px] rounded-lg outline-none border border-gray-500 "/>
-</div>
-<div className="flex flex-col gap-1 md:gap-3 w-full">
-<label htmlFor="email" className="text-[16px] md:text-[20px]">Your Email Address</label>
-  <input type="text" className="w-full h-[30px] rounded-lg outline-none border border-gray-500 "/>
-</div>
-<div className="flex flex-col gap-1 md:gap-3 w-full">
-<label htmlFor="telephone" className="text-[16px] md:text-[20px]">Telephone Number (Optional)</label>
-  <input type="text" className="w-full h-[30px] rounded-lg outline-none border border-gray-500 "/>
-</div>
-<div className="flex flex-col gap-1 md:gap-3 w-full">
-<label htmlFor="" className="text-[16px] md:text-[20px]">What would you like to discuss?</label>
-
-  <textarea name="" id="" cols={30} rows={10} className="outline-none border border-gray-500"></textarea>
-</div>
-<button className="bg-green-800 px-3 py-3 rounded-xl text-white">Send Inquiry</button>
-</div>
-
-<div className="p-2 hidden gap-5 md:flex flex-col md:gap-5 ">
-  <div className="border border-gray-500 w-[300px] h-[250px] rounded-xl">
-    <img src="/care.jpg" alt="" className="w-full h-full bg-cover rounded-xl"/>
-  </div>
-  <div className="border border-gray-500 w-[300px] h-[300px] rounded-xl">
-  <img src="/care2.jpg" alt="" className="w-full h-full  bg-cover rounded-xl"/>
-  </div>
-</div>
-
-
-</div> */}
-
-
-
-
-
-
-
 
 
 
