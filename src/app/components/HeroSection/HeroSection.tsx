64a3a4cfd15c6e5134e1ec77c9d56d8362acc353
imtiaz3699 <imtiazali78652@gmail.com>
2023-08-28 05:32:53 -0700

    changed services icon

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -70,44 +70,52 @@ const HeroSection: React.FC<HeroSectionProps> = ({ showModal, handleModal }) => 
 
   const ourServices = [
     {
-        img:'/wheelchair.png',
-        description:'Assisted living care homes provide support for seniors who need help with daily activities such as bathing, dressing, and medication management. Residents enjoy private apartments and communal spaces while receiving assistance tailored to their individual needs.',
-        name:'Assisted Living Care',
+        // img:'/wheelchair.png',
+        img:'/care-2.png',
+        description:'Trained caregivers provide assistance with activities of daily living such as bathing, dressing, grooming, and toileting, ensuring the individuals personal hygiene and comfort.',
+        name:'Personal Care Assistance',
     },
     {
-        img:'/mental-health.png',
-        description:'For residents requiring specialized care, our dementia and memory care services are designed to provide support, comfort, and a safe environment for individuals living with memory-related conditions.',
-        name:'Dementia and Memory Care: ',
+        // img:'/mental-health.png',
+        img:'/drugs.png',
+        description:'Caregivers help with medication reminders, ensuring that the individual takes the right medication at the correct time, and maintaining accurate records of medication schedules.',
+        name:'Medication Management: ',
     },
     {
-        img:'/respite-care.png',
-        description:'Our modern and well-maintained facilities offer a comfortable and homely atmosphere. Residents can choose from a range of accommodation options that suit their lifestyle and budget, all designed to provide a sense of belonging and security.',
-        name:'Comfortable Accommodation :',
+        // img:'/respite-care.png',
+        img:'/meal.png',
+        description:'Caregivers assist with meal planning, grocery shopping, and cooking nutritious meals tailored to the individuals dietary needs and preferences.',
+        name:'Meal Preparation and Nutrition:',
     },
     {
-        img:'/nursing.png',
+        // img:'/nursing.png',
+        img:'/final-care.png',
         description:'Good nutrition is vital for overall well-being. Our in-house culinary team prepares delicious, balanced meals that cater to individual dietary needs and preferences, ensuring residents receive nourishment that contributes to their health.',
         name:'Nutritious Dining :',
     },
     {
-        img:'/nursing-care.png',
-        description:'Health and safety are paramount. Our skilled medical professionals are available around the clock to provide immediate assistance and care whenever its needed, ensuring peace of mind for both residents and their families.',
-        name:'24/7 Medical Support :',
+        // img:'/nursing-care.png',
+        img:'/love.png',
+        description:'Caregivers offer companionship, engage in conversations, and provide emotional support, reducing feelings of loneliness and fostering a sense of connection.',
+        name:'Companionship and Social Interaction:',
     },
     {
-        img:'/independent-living.png',
-        description:'We understand that every individual has unique needs and preferences. Our experienced team collaborates with residents and their families to create personalized care plans that cater to specific requirements, ensuring that each resident receives the attention they deserve.',
-        name:'Personalized Care Plans :',
+        // img:'/independent-living.png',
+        img:"/solidarity.png",
+        description:'Trained staff help individuals with mobility challenges move around safely, using assistive devices when necessary, and assist with transfers in and out of beds or chairs.',
+        name:'Mobility and Transfer Assistance :',
     },
     {
-        img:'palliative-care.png',
-        description:'We believe in promoting an active and enjoyable lifestyle. Our carefully crafted recreational activities and social events encourage residents to stay engaged, form meaningful connections, and maintain a vibrant sense of community.',
-        name:'Engaging Activities :',
+        // img:'palliative-care.png',
+        img:'/vehicles.png',
+        description:'Caregivers provide transportation to medical appointments, grocery stores, social activities, and run errands to help individuals stay engaged and independent.',
+        name:'Transportation and Errands :',
     },
     {
-        img:'/assistive-care.png',
-        description:'We believe in fostering a strong sense of family and community. Families are encouraged to participate in their loved ones care journey, with regular updates, communication channels, and opportunities to engage in various activities.',
-        name:'Family Involvement :',
+        // img:'/assistive-care.png',
+        img:'/house.png',
+        description:'Respite caregivers offer temporary relief to primary caregivers, allowing them to take a break and recharge while ensuring continuous care for their loved ones.',
+        name:'Respite Care:',
     },
 
 ]
@@ -259,7 +267,7 @@ const ourCare = [
         <div className='grid grid-cols-1 mt-20 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 place-items-center  px-20 gap-20     '>
 {
     ourServices.map((element,idx)=> {
-        return <div className='w-[300px] bg-white text-center px-3 rounded-xl h-[450px] shadow-2xl shadow-slate-700 drop-shadow-2xl border-2  py-5 flex flex-col items-center'>
+        return <div className='w-[300px] bg-white text-center px-3 rounded-xl h-[400px] shadow-2xl shadow-slate-700 drop-shadow-2xl border-2  py-5 flex flex-col items-center'>
         <div className='translate-y-[-50px] bg-blue-900 top-0 rounded-xl w-[70px] h-[70px] flex flex-row items-center justify-center'>
         <img src={element.img} alt="" className="w-[50px] h-[50px] md:w-[50px] md:h-[50px]"/>
         </div>
